9a1467f247ee1076ce6f900ce409fb090e521294
Daiane Bushey <daianebushey@Daianes-MacBook.local>
2016-03-16 21:33:24 -0400

    adding last changes

--- forum/urls.py
+++ forum/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.thread_list, name='thread_list'),
-    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    #url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^thread/(?P<pk>\d+)/$', views.thread_detail, name='thread_detail'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
 
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .models import Thread
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 def thread_list(request):
-    thread = Thread.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'forum/thread_list.html', {'thread': thread})
+    threads = Thread.objects.all()
+    return render(request, 'forum/thread_list.html', {'threads': threads})
+
+def thread_detail(request, pk):
+    thread = get_object_or_404(Thread, pk=pk)
+    posts = Post.objects.filter(thread=thread)
+    return render(request, 'forum/thread_detail.html', {'thread': thread, 'posts': posts})
+    
+def post_list(request):
+    posts = Post.objects.filter(last_edited_date__lte=timezone.now()).order_by('last_edited_date')
+    return render(request, 'forum/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'forum/post_details.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'forum/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.last_edited_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'forum/post_edit.html', {'form': form})
+
+
+
+def user(request):
+    user = User.objects.all()
+    return render(request, 'forum/signin.html', {'user': user})
+
